c63a14af25fb79e82938f8ad541d512c1629549d
Cristian Camilo Osorio <cristian.1701421857@ucaldas.edu.co>
2023-10-13 14:58:18 -0500

    Creacion del metodo de identificacion del usuario

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -16,9 +16,10 @@ import {
   del,
   requestBody,
   response,
+  HttpErrors,
 } from '@loopback/rest';
-import {Usuario} from '../models';
-import {UsuarioRepository} from '../repositories';
+import {Credenciales, Login, Usuario} from '../models';
+import {LoginRepository, UsuarioRepository} from '../repositories';
 import {SeguridadUsuarioService} from '../services/seguridad-usuario.service';
 import {service} from '@loopback/core';
 
@@ -27,7 +28,9 @@ export class UsuarioController {
     @repository(UsuarioRepository)
     public usuarioRepository : UsuarioRepository,
     @service(SeguridadUsuarioService)
-    public servicioSeguridad: SeguridadUsuarioService
+    public servicioSeguridad: SeguridadUsuarioService,
+    @repository(LoginRepository)
+    public repositorioLogin: LoginRepository
   ) {}
 
   @post('/usuario')
@@ -50,7 +53,7 @@ export class UsuarioController {
   ): Promise<Usuario> {
     //CREAR LA CLAVE
 
-    const clave=this.servicioSeguridad.createClave();
+    const clave=this.servicioSeguridad.crearTextoAleatorio(10);
     console.log(clave);
 
 
@@ -165,4 +168,61 @@ export class UsuarioController {
   async deleteById(@param.path.string('id') id: string): Promise<void> {
     await this.usuarioRepository.deleteById(id);
   }
-}
+
+  /**
+   Metodos personalizados para la API
+   */
+
+   @post('/identificar-usuario')
+   @response(200, {
+     description: "Identificar un usuario por correo y clave",
+     content: {'application/json': {schema: getModelSchemaRef(Usuario)}}
+   })
+   async identificarUsuario(
+     @requestBody(
+       {
+         content: {
+           'application/json': {
+             schema: getModelSchemaRef(Credenciales)
+           }
+         }
+       }
+     )
+     credenciales: Credenciales
+   ): Promise<object> {
+     let usuario = await this.servicioSeguridad.identificarUsuario(credenciales);
+     if (usuario) {
+       const codigo2fa = this.servicioSeguridad.crearTextoAleatorio(5);
+       console.log(codigo2fa);
+       const login: Login = new Login();
+       login.usuarioId = usuario._id!;
+       login.codigo2fa = codigo2fa;
+       login.estadoCodigo2fa = false;
+       login.token = "";
+       login.estadoToken = false;
+       // eslint-disable-next-line @typescript-eslint/no-floating-promises
+       this.repositorioLogin.create(login);
+       usuario.clave = "";
+       // notificar al usuario vía correo o sms
+
+
+       return usuario;
+     }
+     return new HttpErrors[401]("Credenciales incorrectas.");
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+
+
--- src/services/seguridad-usuario.service.ts
+++ src/services/seguridad-usuario.service.ts
@@ -1,4 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+import {repository} from '@loopback/repository';
+import {UsuarioRepository} from '../repositories';
+import {Credenciales} from '../models/credenciales.model';
+import {Usuario} from '../models';
 const generator = require('generate-password');
 const md5 = require("crypto-js/md5");
 
@@ -8,13 +13,25 @@ const md5 = require("crypto-js/md5");
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class SeguridadUsuarioService {
-  constructor(/* Add @inject to inject parameters */) {}
+  constructor(
+    @repository(UsuarioRepository)
+    public repositorioUsuario: UsuarioRepository
+  ) {}
 
   /*
    * Add service methods here
    */
 
-  createClave():string{
+
+  /**
+   * Crear una clave aleatoria
+   * @returns cadena aletoria de n caracteres
+   *
+   */
+
+
+
+  crearTextoAleatorio(n:number):string{
 
     const clave = generator.generate({
       length: 10,
@@ -27,10 +44,48 @@ export class SeguridadUsuarioService {
 
   }
 
+  /**
+   * Cifrar una cadena de texto con metodo md5
+   * @param cadena texto a cifrar
+   * @returns cadena cifrada con md5
+   */
+
   cifrarTexto(cadena: string):string{
     const cadenaCifrada=md5(cadena).toString();
 
     return cadenaCifrada;
 
-}}
+}
+
+/**
+ * Se busca un usuario por sus credenciales de acceso
+ * @param credenciales credenciales del usuario
+ * @returns usuario encontrado o null
+ */
+
+async identificarUsuario(credenciales:Credenciales):Promise <Usuario | null>{
+  const usuario=await this.repositorioUsuario.findOne(
+    {
+
+      where:{
+        correo:credenciales.correo,
+        clave:credenciales.clave
+      }
+
+
+    }
+  );
+
+  return usuario as Usuario;
+
+
+
+
+}
+
+
+
+
+
+}
 
